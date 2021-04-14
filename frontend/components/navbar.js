274b80ca876317d573fd20d1ad5c0af90f7e08aa
sunarmzaa <sunarmzaa@gmail.com>
2021-04-15 01:29:21 +0700

    two

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,18 +1,31 @@
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 const Navbar = () => (
-    <div>
-        <Link href="/"><a> Home </a></Link> |
-        <Link href="/register"><a> Register </a></Link>  |
-        <Link href="/login"><a> Login </a></Link> |
-        <Link href="/profile"><a> Profile </a></Link> | 
-        <Link href="/foo"><a> Foo </a></Link> |
-        <Link href="/students"><a> Sudents </a></Link> |
-        <Link href="/admin"><a> Admin </a></Link> |
-        <Link href="/getConfig"><a> Config </a></Link> | 
-        <Link href="/logout"><a> Logout </a></Link> 
-        
-    </div>
+
+        <div className={styles.nav}>
+            <ul>
+                <li><Link href="/"><a> Home </a></Link> </li>
+                <li><Link href="/register"><a> Register </a></Link></li>
+                <li><Link href="/login"><a> Login </a></Link> </li>
+                <li><Link href="/profile"><a> Profile </a></Link> </li>
+                <li><Link href="/students"><a> Sudents </a></Link> </li>
+                <li><Link href="/admin"><a> Admin </a></Link> </li>
+                <li><Link href="/logout"><a> Logout </a></Link> </li>
+            </ul>
+        </div>
 )
 
 export default Navbar
+
+/*
+ <Link href="/"><a> Home </a></Link>
+        <Link href="/register"><a> Register </a></Link>
+        <Link href="/login"><a> Login </a></Link>
+        <Link href="/profile"><a> Profile </a></Link>
+        <Link href="/foo"><a> Foo </a></Link>
+        <Link href="/students"><a> Sudents </a></Link>
+        <Link href="/admin"><a> Admin </a></Link>
+        <Link href="/getConfig"><a> Config </a></Link>
+        <Link href="/logout"><a> Logout </a></Link>
+        */
